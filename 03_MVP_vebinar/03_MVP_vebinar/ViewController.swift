8de3180521918e53c8421dac59e6d52a52e01757
fedir72 <fedir72@ua.fm>
2020-10-01 08:55:46 +0300

    start project

---
+++ 03_MVP_vebinar/03_MVP_vebinar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  03_MVP_vebinar
+//
+//  Created by fedir on 01.10.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
